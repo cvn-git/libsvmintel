40f3c262a9c38d5084d39ed202fb50d69d0fdecf
Cong-Van Nguyen <cong.nguyen@sonartech.com.au>
2019-09-13 12:52:22 +1000

    pip install should work in Linux now

--- pip_install.sh
+++ pip_install.sh
@@ -3,7 +3,7 @@ set -e
 export BIN_DIR="$PWD/bin"
 #echo "BIN_DIR = $BIN_DIR" > cmake_build.log
 rm -rf "$BIN_DIR"
-cmake -H. -B"$BIN_DIR" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBRARY
+cmake -H. -B"$BIN_DIR" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBRARY=1
 cd "$BIN_DIR"
 make
 cd ..
