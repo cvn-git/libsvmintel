1d26ba7165fdd024e5b959bf00a35d80c1f86970
Cong-Van Nguyen <cong.nguyen@sonartech.com.au>
2019-09-13 13:00:57 +1000

    Better error handling for the batch file

--- pip_install.bat
+++ pip_install.bat
@@ -2,14 +2,11 @@ call %1 %2
 set BIN_DIR=bin
 rmdir /q/s %BIN_DIR%
 
-cmake -H. -B%BIN_DIR% -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBRARY=1 -G "NMake Makefiles"
-IF %ERRORLEVEL% NEQ 0 ( 
-   EXIT /B %ERRORLEVEL%
-)
+cmake -H. -B%BIN_DIR% -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBRARY=1 -G "NMake Makefiles" || goto :error
 
 cd %BIN_DIR%
-nmake
-IF %ERRORLEVEL% NEQ 0 ( 
-   EXIT /B %ERRORLEVEL%
-)
+nmake || goto :error
 cd ..
+
+:error
+exit /b %errorlevel%
